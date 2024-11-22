a16e952c91c9e88db9b60ad5da406e5f6aa39cdc
br3aded <kelseyRholdgate@icloud.com>
2024-11-22 16:16:27 +0000

    Spec

---
+++ q1answer.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+ 
+# ======= Question 1 - OpenSSL =======
+# Please provide the full commands as well as any outputs in this file, and
+# submit it along with the PDF file containing the answers to other questions.
+
+# Commands should be provided on new lines; output should be included as
+# comments. You can write a comment by prefacing it with a # symbol
+
+# Example Command:
+echo 'Hello world'
+# Example Output:
+# Hello world
+
+# Example Command:
+openssl help
+# Example Output:
+# help:
+#
+# Standard commands
+# asn1parse         ca                ciphers           cmp
+# cms               crl               crl2pkcs7         dgst
+# dhparam           dsa               dsaparam          ec
+# ecparam           enc               engine            errstr
+# [...and so on]
+
+# If you used any resources to complete the questions, please list them at the bottom of the file, and include a [1],
+# [2] etc. next to the relevant question's answer/output.
+# For example, if you used https://www.example.com to answer Q1.a, you would write:
+# Q1.a Output [1]
+# OpenSSL 1.1.1
+# and then at the bottom of the file:
+# [1] https://www.example.com
+
+# ======= Please provide your answers below: ========
+# Q1.a Command:
+
+# Q1.a Output
+#
+
+# Q1.b Command:
+
+# Q1.b Output
+#
+
+# Q1.c Command:
+
+# Q1.c Output
+#
+
+# Q1.d Command:
+
+# Q1.d Output
+#
+
+# Q1.e Command:
+
+# Q1.e Output
+#
+
+# Q1.f Command:
+
+# Q1.f Output
+#
+
+# Q1.g Answers
+# When  was  the  timestamp  created?
+#
+# What  does  that  imply  about  the  key  file?
+#
+
+# Q1.h Command:
+
+# Q1.h Output
+#
+
+# Q1.i Commands:
+
+# Q1.i Output
+#
+#
+# Cert 1 - Valid/Invalid
+#
+# Cert 2 - Valid/Invalid
+#
+# Cert 3 - Valid/Invalid
+#
+
+# Sources:
+# [1]
+# [2]
+# [3]
